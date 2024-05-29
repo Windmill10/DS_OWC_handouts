ba03ab81d8023a601871a83dd21bb1925e3bae4c
Windmill10 <eason200506@gmail.com>
2024-05-29 13:41:37 +0800

    updated script

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Loop over the range 2770 to 2815
-for i in {2770..2815}; do
-  wget -r -A.pdf "https://ocw.nthu.edu.tw/ocw/index.php?page=mobile&type=chapter&cid=252&chid=${i}"
+# Loop over the range 2770 to 2830 
+for i in {2770..2830}; do
+  # Use curl to download the content
+  wget -r -A.pdf  "https://ocw.nthu.edu.tw/ocw/index.php?page=mobile&type=chapter&cid=252&chid=${i}"
 done
